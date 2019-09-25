c33770c735b34a41d6d1fb046c7f227ec20f8fb2
matthew rybak <mrybak1991@gmail.com>
2019-09-25 19:35:06 -0400

    final commit

--- models/index.js
+++ models/index.js
@@ -1,4 +1,3 @@
-// Exporting an object containing all of our models
 
 module.exports = {
     Article: require("./Article"),
--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,9 +1,6 @@
-// Grab the articles as a json
 $.getJSON("/articles", function(data) {
-    // For each one
     for (var i = 0; i < data.length; i++) {
         var articleURL = "https://www.reuters.com"+ data[i].link;
-      // Display the apropos information on the page
       $("#scrape-results").append("<p data-id='" + data[i]._id + "'>" + data[i].title + 
       "<button class='save-article button is-info is-medium' data-id='" + data[i]._id + "'><span class='icon'><i class='fa fa-bookmark'></i></span>Save Article</button>"
       + "<br />" + data[i].summary + "<br />" + articleURL + "</p>" + "<br><br>");
